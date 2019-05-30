d4219e55d16393b699322d7b3315ae0397de4ff6
qilianshan <qilianshan@xiaomi.com>
2019-05-30 15:56:46 +0800

    rebuild
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/com/scala/Util.scala
+++ src/com/scala/Util.scala
@@ -3,7 +3,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 object Util {
   def getSc(): SparkContext ={
     val conf=new SparkConf().setAppName("mySpark")
-    conf.setMaster("local")
+    conf.setMaster("local[2]")
+//    conf.setMaster("spark://qilianshans-iMac.local:7077")
+    //spark.master  spark://qilianshans-iMac.local:7077
+    System.setProperty("hadoop.home.dir","/Users/qilianshan/Documents/env/hadoop-2.9.2");
     val sc=new SparkContext(conf)
     return sc
   }
