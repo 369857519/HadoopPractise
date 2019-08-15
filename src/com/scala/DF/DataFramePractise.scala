b70bbb8653f25ec548da29838b722098f02b0d12
haoyijing <xzhyj93@gmail.com>
2019-08-15 09:04:16 +0800

    year
    Signed-off-by: haoyijing <xzhyj93@gmail.com>

--- src/com/scala/DF/DataFramePractise.scala
+++ src/com/scala/DF/DataFramePractise.scala
@@ -18,7 +18,7 @@ object DataFramePractise {
       inc =>
         Incedents(inc(0), inc(1), inc(2), inc(3), inc(4), inc(5), inc(6).toString, inc(7).toString, inc(8).toString, inc(9).toString, inc(10).toString, inc(11).toString, inc(12).toString)
     }.toDF()
-    //    sfpdDF.show(10)
+    sfpdDF.show(10)
     //    sfpdCase.describe().show()
     //    sfpdDF.printSchema()
     //取top3
@@ -27,15 +27,23 @@ object DataFramePractise {
     val numAddDesc = numAdd.sort($"count".desc)
     sfpdDF.createOrReplaceTempView("sfpd");
     numAddDesc.show(3)
-
+    val sqlContext = sfpdDF.sqlContext
     val top3Address = sfpdDF.sqlContext.sql("SELECT address,count(incidentnum) as inccount" +
       " FROM sfpd GROUP BY address ORDER BY inccount DESC LIMIT 3").show
 
     //自定义函数
-    val getYearString =  (s: String) => {
+    val getYearString = (s: String) => {
       val lastString = s.substring(s.lastIndexOf('/) + 1)
     }
-    sfpdDF.sqlContext.udf.register("getYearString",getYearString)
-//    val year=sfpdDF.groupBy("Date",getYearString($"Date"))
+
+    //    sfpdDF.sqlContext.udf.register("getYearString",getYearString)
+    //    val year=sfpdDF.groupBy(getYearString(sfpdDF("Date"))).count.show
+    def getString(s: String) = {
+      val stringAfter = s.substring(s.lastIndexOf('/') + 1); stringAfter
+    }
+
+    sqlContext.udf.register("getString", getString _)
+    val numOfIncByYear = sfpdDF.sqlContext.sql("SELECT getString(date), count(incidentnum) AS countbyyear FROM sfpd GROUP BY getString(date) ORDER BY countbyyear DESC LIMIT 5");
+    numOfIncByYear.show()
   }
 }
